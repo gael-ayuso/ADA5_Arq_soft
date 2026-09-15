af5a8fd46a56163c14ab70c7c1092a43ff899655
criscnsn <santanacristopher765@gmail.com>
2026-09-15 00:28:03 -0600

    Implement VerificarFraude filter to flag orders with subtotal > $5,000.

---
+++ src/main/java/filtros/VerificarFraude.java
@@ -0,0 +1,24 @@
+package filtros;
+
+import models.EstadoPedido;
+import models.Pedido;
+
+public class VerificarFraude implements Filtro {
+    private static final double LIMITE_FRAUDE=5000.0;
+
+    @Override
+    public Pedido procesar(Pedido pedido) {
+        if (pedido == null){
+            return null;
+        }
+
+        if (pedido.getSubtotal() > LIMITE_FRAUDE) {
+            pedido.setRevisionFraude(true);
+            pedido.setEstado(EstadoPedido.PEDIDO_MARCADO_COMO_FRAUDE);
+        }else {
+            pedido.setRevisionFraude(false);
+        }
+
+        return pedido;
+    }
+}
