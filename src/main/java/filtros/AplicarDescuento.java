b4f157afee89a98680a0c337da165cdac95a83f0
criscnsn <santanacristopher765@gmail.com>
2026-09-15 00:05:01 -0600

    Implement AplicarDescuento filter to grant 10% discount for orders >= $1,000.

--- src/main/java/filtros/AplicarDescuento.java
+++ src/main/java/filtros/AplicarDescuento.java
@@ -1,10 +1,30 @@
 package filtros;
 
+import models.EstadoPedido;
 import models.Pedido;
 
 public class AplicarDescuento implements Filtro{
+    //estos 2 parametros son una base pues no se especifica exactamente
+    //cual es el descuento ni la cantidad exacta para obtenerlo
+    private static final double UMBRAL_DESCUENTO = 1000.0;
+    private static final double PORCENTAJE_DESCUENTO= 0.1;
+
     @Override
     public Pedido procesar(Pedido pedido) {
-        return null;
+        if (pedido ==null ){
+            return null;
+        }
+
+        if (pedido.getSubtotal() >= UMBRAL_DESCUENTO) {
+            double descuento = pedido.getSubtotal() * PORCENTAJE_DESCUENTO;
+
+            pedido.setDescuento(descuento);
+            pedido.setEstado(EstadoPedido.PEDIDO_CON_DESCUENTO);
+        } else {
+            pedido.setDescuento(0);
+            pedido.setEstado(EstadoPedido.PEDIDO_SIN_DESCUENTO);
+        }
+
+        return pedido;
     }
 }
