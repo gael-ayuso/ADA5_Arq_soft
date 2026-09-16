4e8ab9d54ba76765a9615c20ead92614a9c378d4
MateoAlejandroCaamalTencle <A24216394@alumnos.uady.mx>
2026-09-15 20:14:58 -0600

    Update CalcularSubtotal.java

--- src/main/java/filtros/CalcularSubtotal.java
+++ src/main/java/filtros/CalcularSubtotal.java
@@ -1,10 +1,21 @@
 package filtros;
 
 import models.Pedido;
+import models.Producto;
 
 public class CalcularSubtotal implements Filtro{
     @Override
     public Pedido procesar(Pedido pedido) {
-        return null;
+        if (pedido == null || pedido.getListaProductos() == null) {
+            return pedido;
+        }
+
+        double subtotal = 0.0;
+        for (Producto producto : pedido.getListaProductos()) {
+            subtotal += (producto.getPrecio() * producto.getCantidad());
+        }
+
+        pedido.setSubtotal(subtotal);
+        return pedido;
     }
 }
